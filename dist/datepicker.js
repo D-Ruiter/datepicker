51d3e3898bca98dc4577d702fbbd9cd23d0ce14a
Dennis Ruiter <dennis.ruiter@tradecast.eu>
2015-12-09 12:00:02 +0100

    new dist

--- dist/datepicker.js
+++ dist/datepicker.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2014-2015 Fengyuan Chen
  * Released under the MIT license
  *
- * Date: 2015-12-09T10:57:55.993Z
+ * Date: 2015-12-09T10:59:48.516Z
  */
 
 (function (factory) {
@@ -339,8 +339,6 @@
         }
 
         $this.val(val);
-
-        console.log(parseInt(val, 10));
 
         self.date.setMinutes(parseInt(val, 10));
         self.viewDate.setMinutes(parseInt(val, 10));
@@ -1141,11 +1139,9 @@
     // Update the datepicker with the current input value
     update: function () {
       this.setDate(this.getValue(), true);
-      console.log(this.date, this.viewDate);
+
       var h = this.viewDate.getHours(),
           m = this.viewDate.getMinutes();
-
-      console.log(h, m);
 
       $('input[data-hours]').val(h < 10 ? '0' + h : h);
       $('input[data-minutes]').val(m < 10 ? '0' + m : m);
